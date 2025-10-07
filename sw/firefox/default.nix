8295e6c8160bf0c2fb6f5b24d6f3188fd57743d0
exclusive-and <exclusiveandgate@gmail.com>
2025-10-07 11:53:13 -0400

    Updated Firefox configuration

--- sw/firefox/default.nix
+++ sw/firefox/default.nix
@@ -1,28 +1,27 @@
 { config, pkgs, ... }:
 
-let
-    extensions = with pkgs.firefox-addons; [
-        dashlane
-        toggl-button-time-tracker
-        ublock-origin
-    ];
-
-    settings = {
-        "browser.tabs.closeTabByDblclick" = true;
-        "sidebar.revamp" = true;
-        "sidebar.verticalTabs" = true;
-    };
-in
 {
-    programs.firefox = {
-        enable = true;
-        package = pkgs.firefox;
-        profiles = {
-            default = {
-                id = 0;
-                path = "2d43dm8x.default";
-                inherit extensions settings;
-            };
+  programs.firefox = {
+    enable = true;
+    package = pkgs.firefox;
+    
+    profiles = {
+      default = {
+        id = 0;
+        extensions = {
+          packages = with pkgs.firefox-addons; [
+            dashlane
+            toggl-button-time-tracker
+            ublock-origin
+          ];
         };
+        path = "2d43dm8x.default";
+        settings = {
+            "browser.tabs.closeTabByDblclick" = true;
+            "sidebar.revamp" = true;
+            "sidebar.verticalTabs" = true;
+        };
+      };
     };
+  };
 }
